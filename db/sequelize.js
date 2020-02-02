00d34733e4b7393b3d9fafeffb8a4dcdb0e49e19
John Lee <johnjun.lee91@gmail.com>
2020-02-02 02:02:31 -0500

    Setup sequelize database

---
+++ db/sequelize.js
@@ -0,0 +1,80 @@
+const Sequelize = require('sequelize');
+const config = require('../config');
+
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  dialect: 'mysql'
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('***CONNECTED TO SEQUELIZE***');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+  const LTC = sequelize.define('LTCs', {
+    date: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: 'LTCs_unique'
+    },
+    price_close: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false
+    }
+}, {
+    define: {
+        timestamps: false
+    }
+});
+
+  const ETH = sequelize.define('ETHs', {
+    date: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: 'ETHs_unique'
+    },
+    price_close: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false
+    }
+}, {
+    define: {
+        timestamps: false
+    }
+});
+
+const CurrentPrices = sequelize.define('CurrentPrices', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: 'CurrentPrices_unique'
+  },
+  date: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  USD: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: false
+  },
+  EUR: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: false
+  },
+  GBP: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: false
+  }
+});
+
+CurrentPrices.sync({force: false});
+// LTC.sync({force: true});
+// ETH.sync({force: true});
+
+module.exports.LTC = LTC;
+module.exports.ETH = ETH;
+module.exports.CurrentPrices = CurrentPrices;
