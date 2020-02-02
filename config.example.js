9de7c06e352d7daee80f05f63fbaeff2bb6dddb3
John Lee <johnjun.lee91@gmail.com>
2020-02-02 16:06:12 -0500

    Added config example file

---
+++ config.example.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: 'local',
+    database: 'coinchase',
+    username: 'INSERT_MYSQL_USERNAME',
+    password: 'INSERT_MYSQL_PASSWORD',
+    apiKey: `INSERT_APIKEY_HERE`
+}
