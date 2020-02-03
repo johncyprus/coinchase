378cb0a2a22642760880169e982e0796d53da6f4
John Lee <johnjun.lee91@gmail.com>
2020-02-02 22:16:49 -0500

    Update dependencies and webpack config for css handling

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,29 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  'style-loader',
+                  'css-loader'
+                ]
+            },
+            { 
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+                loader: "url-loader?limit=10000&mimetype=application/font-woff" 
+            },
+            { 
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+                loader: "file-loader" 
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  }
+                ]
             }
         ]
     },
